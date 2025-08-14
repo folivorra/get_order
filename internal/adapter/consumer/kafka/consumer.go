fc3c00a2579d75bff4d26d628f34dca8e2460403
Emil Yay <keyt.isac@yandex.ru>
2025-08-14 05:36:59 +0300

    refactor: retry deleted bc was useful for b-logic

--- internal/adapter/consumer/kafka/consumer.go
+++ internal/adapter/consumer/kafka/consumer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/brianvoe/gofakeit/v7"
 	"github.com/folivorra/get_order/internal/adapter/mapper"
 	"github.com/folivorra/get_order/internal/config"
+	"github.com/folivorra/get_order/internal/repository/postgres"
 	"github.com/folivorra/get_order/internal/usecase"
 	"github.com/segmentio/kafka-go"
 	"log/slog"
@@ -14,7 +15,7 @@ import (
 )
 
 type Consumer struct {
-	logger slog.Logger
+	logger *slog.Logger
 	cfg    config.Config
 	reader *kafka.Reader
 	srv    *usecase.OrderService
@@ -22,7 +23,7 @@ type Consumer struct {
 
 func NewConsumer(logger *slog.Logger, cfg config.Config, reader *kafka.Reader, srv *usecase.OrderService) *Consumer {
 	return &Consumer{
-		logger: *logger,
+		logger: logger,
 		cfg:    cfg,
 		reader: reader,
 		srv:    srv,
@@ -64,55 +65,47 @@ func (c *Consumer) Start(ctx context.Context) {
 				c.logger.Error("failed to read message, retrying",
 					slog.String("error", err.Error()),
 				)
+				jitter := time.Duration(gofakeit.IntN(500)) * time.Millisecond
+				time.Sleep(c.cfg.KafkaBackoff + jitter)
 				continue
 			}
 
 			var orderDTO mapper.OrderIntoDomainDTO
-			commit := false
 
 			if err = json.Unmarshal(msg.Value, &orderDTO); err != nil {
-				commit = true
 				c.logger.Error("failed to unmarshal message",
 					slog.String("error", err.Error()),
 				)
-			} else {
-				order := mapper.ConvertToDomain(&orderDTO)
-				err = c.srv.ProcessIncomingOrder(ctx, order)
-
-				switch {
-				case errors.Is(err, usecase.ErrOrderAlreadyExists):
-					commit = true
-					c.logger.Warn("order already exists",
-						slog.String("uuid", order.OrderUID.String()),
-					)
-				case err != nil:
-					c.logger.Error("failed to process order, retrying",
-						slog.String("uuid", order.OrderUID.String()),
-						slog.String("error", err.Error()),
-					)
-
-					jitter := time.Duration(gofakeit.IntN(500)) * time.Millisecond
-
-					time.Sleep(c.cfg.KafkaBackoff + jitter)
-				default:
-					commit = true
-					c.logger.Info("order has been saved in db",
-						slog.String("uuid", order.OrderUID.String()),
-					)
-
-					orderView, _ := json.MarshalIndent(orderDTO, "", "	")
-					c.logger.Debug("saved order view",
-						slog.String("order", string(orderView)),
-					)
-				}
 			}
 
-			if commit {
-				if err = c.reader.CommitMessages(ctx, msg); err != nil {
-					c.logger.Error("failed to commit message",
-						slog.String("error", err.Error()),
-					)
-				}
+			order := mapper.ConvertToDomain(&orderDTO)
+			err = c.srv.ProcessIncomingOrder(ctx, order)
+
+			switch {
+			case errors.Is(err, postgres.ErrOrderAlreadyExists):
+				c.logger.Warn("order already exists",
+					slog.String("uuid", order.OrderUID.String()),
+				)
+			case err != nil:
+				c.logger.Error("failed to process order",
+					slog.String("uuid", order.OrderUID.String()),
+					slog.String("error", err.Error()),
+				)
+			default:
+				c.logger.Info("order has been saved in db",
+					slog.String("uuid", order.OrderUID.String()),
+				)
+
+				orderView, _ := json.MarshalIndent(orderDTO, "", "	")
+				c.logger.Debug("saved order view",
+					slog.String("order", string(orderView)),
+				)
+			}
+
+			if err = c.reader.CommitMessages(ctx, msg); err != nil {
+				c.logger.Error("failed to commit message",
+					slog.String("error", err.Error()),
+				)
 			}
 		}
 	}
